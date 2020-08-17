08440934c726c07684b51be0f479d7b25d424053
Robinson Ngecu <ngecu16@gmail.com>
2020-08-17 09:38:35 +0300

    Update models.py

--- Stepped-Solutions/2.Database Models $ Admin Panel/mysite/main/models.py
+++ Stepped-Solutions/2.Database Models $ Admin Panel/mysite/main/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from datetime import datetime
 
 class TutorialSeries(models.Model):
-	tutorial_series = models.CharField(max_length=200,blank=True, null=True)
+	tutorial_series_name = models.CharField(max_length=200,blank=True, null=True)
 	series_summary = models.CharField(max_length=200,blank=True, null=True)
-	series_image = models.URLField(max_length=200,blank=True, null=True)
+# 	series_image = models.URLField(max_length=200,blank=True, null=True)
 	source_code = models.URLField(max_length=200,blank=True, null=True)
 	video_url = models.URLField(max_length=200,blank=True, null=True)
 	series_published = models.DateTimeField("date published", default=datetime.now(),blank=True, null=True)
@@ -34,7 +34,7 @@ class Subscription(models.Model):
 		return self.email	
 	
 class Tutorial(models.Model):
-	tutorial_series = models.ForeignKey(TutorialSeries, on_delete=models.CASCADE,blank=True, null=True)
+	#tutorial_series = models.ForeignKey(TutorialSeries, on_delete=models.CASCADE,blank=True, null=True)
 	tutorial_title = models.CharField(max_length=200)
 	tutorial_content = models.TextField()
 	tutorial_published = models.DateTimeField("date published", default=datetime.now(),blank=True, null=True)
@@ -45,11 +45,11 @@ class Tutorial(models.Model):
 
 
 class Comment(models.Model):
-	tutorial = models.ForeignKey(TutorialSeries,on_delete=models.CASCADE,related_name='comments',blank=True, null=True)
-	name = models.CharField(max_length=80,blank=True, null=True)
-	email = models.EmailField(blank=True, null=True)
-	body = models.TextField(blank=True, null=True)
-	created_on = models.DateTimeField(auto_now_add=True,blank=True, null=True)
+	#tutorial = models.ForeignKey(TutorialSeries,on_delete=models.CASCADE,related_name='comments',blank=True, null=True)
+	commentor_name = models.CharField(max_length=80,blank=True, null=True)
+	commentor_email = models.EmailField(blank=True, null=True)
+	comment_body = models.TextField(blank=True, null=True)
+	comment_date = models.DateTimeField(auto_now_add=True,blank=True, null=True)
 
 
 
@@ -60,18 +60,18 @@ class Comment(models.Model):
 		return 'Comment by {} of {}-{}'.format(self.name, self.email,self.created_on)
 
 
-class Like(models.Model):
-    comment = models.ForeignKey(Comment,null=True,on_delete=models.SET_NULL)
+# class Like(models.Model):
+#     comment = models.ForeignKey(Comment,null=True,on_delete=models.SET_NULL)
 
-    def __str__(self):
-        return str(self.comment)
+#     def __str__(self):
+#         return str(self.comment)
 
 
-class Dislike(models.Model):
-    comment = models.ForeignKey(Comment,null=True,on_delete=models.SET_NULL)
+# class Dislike(models.Model):
+#     comment = models.ForeignKey(Comment,null=True,on_delete=models.SET_NULL)
 
-    def __str__(self):
-        return str(self.comment)
+#     def __str__(self):
+#         return str(self.comment)
 
 
 
